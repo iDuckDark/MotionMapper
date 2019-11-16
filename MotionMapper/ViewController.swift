2706eb88383450b835c4de0619e00ab53df8c2ff
iDuckDark <nevinganesan@hotmail.com>
2019-11-16 14:18:39 -0500

    Initial Commit

---
+++ MotionMapper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MotionMapper
+//
+//  Created by iDarkDuck on 2019-11-16.
+//  Copyright © 2019 iDarkDuck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
